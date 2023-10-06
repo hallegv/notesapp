6d530aed73bf94b8e61338e429e702b2dd4c42cc
hallegv <hallevogelpohl@gmail.com>
2023-10-05 17:37:49 -0700

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Halle V` on 10/5/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
